e80a37f052e74dff8c4abff52ed9b2673a38e56a
Fabio Caires <fabiocaires@gmail.com>
2016-07-24 22:42:44 -0300

    First
    First commit, just starting

---
+++ logstorage.js
@@ -0,0 +1,26 @@
+/* 
+*
+*  logStorage.js v0.0.1
+*  A micro library which make the debugging easier while keep the console clean
+*  
+*  By Fabio Caires <fabiocaires@gmail.com> under the license CC0
+*
+*/
+
+(function(){
+
+    var data = [];
+
+    var $log = (function(window, undefined) {
+
+        function add(index, value, fontColor, backgroundColor){
+
+        }
+
+        return {
+            add: add
+        }
+
+    });
+
+})(window);
